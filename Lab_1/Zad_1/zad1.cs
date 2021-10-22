5cb4fdf13c4ef4a0caafe6d9861e2881c5414828
Adam Welnicki <adam.welnicki@gmail.com>
2021-10-22 22:25:24 +0200

    Lab_1 (zad1 i zad2)

---
+++ Lab_1/Zad_1/zad1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj temperaturę w stopniach Celsjusza: ");
+            int c = Int32.Parse(Console.ReadLine());
+            double f = 32 + (9/5.0*c);
+            Console.WriteLine("Temperatura w stopniach Fahrenheita: {0:F1}°", f);
+            var machine = System.Environment.MachineName;
+            Console.WriteLine("Dziękuję " + machine + ". Naciśnij przycisk, by zakończyć.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_1/Zad_2/zad2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var machine = System.Environment.MachineName; // Nazwa komputera użytkownika
+            float a, b, c;
+            double delta;
+            Console.WriteLine("Obliczę deltę!");
+            Console.Write("Podaj współczynnik a: ");
+            a = float.Parse(Console.ReadLine());
+            Console.Write("Podaj współczynnik b: ");
+            b = float.Parse(Console.ReadLine());
+            Console.Write("Podaj współczynnik c: ");
+            c = float.Parse(Console.ReadLine());
+
+            delta = Math.Pow(b, 2) - 4*a*c;
+            Console.WriteLine("Delta wynosi: {0:F2}", delta);
+            Console.WriteLine("Dziękuję " + machine + ". Naciśnij przycisk, by zakończyć.");
+            Console.ReadKey();
+        }
+    }
+}
